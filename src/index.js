af68ca0d24364a79e0748266c5a449dae9755d41
rmahawewa <rmahawewa@yahoo.com>
2024-10-02 21:25:15 +0530

    implement functions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-localStorage.setItem("name","Ravini");
-console.log(localStorage.getItem("name"));
+// import json from './todo_details.json';
+const json = require('./todo_details.json');
+
+console.log(json[1]);
+
+for (const entry of json){
+    console.log(entry.name);
+}
+// console.log(JSON.stringify(json));
 
--- src/user_functions.js
+++ src/user_functions.js
@@ -1,13 +1,59 @@
-function create_user(name, password){
-    // let user_string = '"name": ' + '"'+ name + '" , "password": ' + '"' + password +'",';
+const json = require('./todo_details.json');
+
+//this function is to check user name availability
+function check_user_availability(username){
+    for(let entry of json){
+        let name = entry.name;
+        if(username.localeCompare(name) === 0){
+            return false;
+        }
+    }
+    return true;
+}
+
+// this function valiates user login. If user is a valid user function returns the user object index. If user is not valid function 
+// returns -1
+
+function validate_user_login(username, password){
+    let count = 0;
+    for(let entry of json){
+        let name = entry.name;
+        let password = entry.password;
+        if(username.localeCompare(name) === 0 && password.localeCompare(password) === 0){
+            return count;
+        }
+        count++;
+    }
+    return -1;
+}
+
+//
+
+
+// this function is to create a new user object
+function create_user(name, password, date_created, time_created){
     let usr_obj = {
         name: name,
         password: password,
+        account_date: date_created,
+        account_time: time_created,
+        projects: [
+            {
+                project_name: "Default project",
+                project_description: "This is the default project",
+                project_start_date: "2024-09-15",
+                project_end_date: "2026-04-10",
+                project_priority: "medium",
+                project_is_completed: "no",
+                project_completed_date: "",
+                todos: [],
+            },
+        ],
     };
-
     return usr_obj;
 }
 
+// this function is to create a new project
 function create_project(name, description, date_of_start, due_date, priority, is_completed, completed_date){
     let project_obj = {
         project_name: name,
@@ -25,10 +71,12 @@ function create_project(name, description, date_of_start, due_date, priority, is
     return {project_obj};
 }
 
+//this function is to add a todo into a project
 function add_todo_to_project(project_obj, todo_obj){
     project_obj.todos.push(todo_obj);
 }
 
+//this function is to create a todo
 function create_todo(name, description, start_date, end_date, start_time, end_time, priority, is_completed, completed_date, completed_time){
     let todo_obj = {
         todo_name: name,
